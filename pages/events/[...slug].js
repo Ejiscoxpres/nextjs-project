0204b44f21a8c4b22e38017f2a32f4270af60187
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-12 18:04:35 +0100

    final steps

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,18 +1,25 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { getFilteredEvents } from "../dummy-data";
+import Eventlist from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import { Fragment } from "react";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
 
-export default function FilteredEventPage(){
+export default function FilteredEventPage(filteredEvents){
 
     const router =useRouter(); 
     const filterData = router.query.slug;
+
     if (!filterData){
         return <p className="center">Loading...</p>;
+    }
 
         const filteredYear = filterData[0];
         const filteredMonth = filterData[1];
 
-        const numYear = +filteredYear;
-        const numMonth = +filteredMonth;
+        const numYear = + filteredYear;
+        const numMonth = + filteredMonth;
 
         if(
             isNaN(numYear)||
@@ -22,21 +29,49 @@ export default function FilteredEventPage(){
             numMonth < 1 ||
             numMonth > 12
         ) {
-            return <p>Invlid filter, please adjust your values!</p>;
-            const getFilteredEvents = getFilteredEvents({
+            return <Fragment>
+
+                <ErrorAlert>
+                <p>Invlid filter, please adjust your values!</p>;
+                </ErrorAlert>
+
+                <div className="center"> 
+                            <Button link ='/events'>Show All Buttons</Button>
+                          </div>
+            </Fragment>
+
+            const filteredEvents = getFilteredEvents({
                 year: numYear,
                 month: numMonth,
             });
+        }
 
-            if(!filterEvents || filterEvents.length === 0){
-                <p>No event found for the chosen filter!</p>;
+
+            if(!filteredEvents || filteredEvents.length === 0){
+                return(
+                    <Fragment>
+                          <ErrorAlert>
+                          <p>No event found for the chosen filter!</p>;
+                          </ErrorAlert>
+
+                          <div className="center"> 
+                            <Button link ='/events'>Show All Buttons</Button>
+                          </div>
+                         
+
+                    </Fragment>
+                )
+              
             }
 
-        }
-    }
+       
+
+        const date = new Date(numYear, numMonth -1);
+    
     return(
-        <div>
-            <h1>Filtered Event</h1>
-        </div>
+        <Fragment>
+            <ResultsTitle date ={date}/>
+           <Eventlist items = {filteredEvents} />
+        </Fragment>
     )
 }
